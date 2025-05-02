1076ad6df8bf4d057c446cea791f2c7dbcfac5eb
subhan-skysys <muhammad.subhan@myskysys.com>
2025-05-02 19:46:51 +0500

    add an option to play again

--- index.py
+++ index.py
@@ -1,16 +1,36 @@
 import random
 
-secret_number = random.randint(1,100)
-attemps = 0
+play = True
 
-while True:
-    guess = int(input("Guess a number between 1 and 100: "))
-    attemps += 1
+while play:
     
-    if guess < secret_number: 
-        print("Too Low! Try Again")
-    elif guess > secret_number:
-        print("Too High! Try Again")
-    else:
-        print(f"Congratulations you guess it in {attemps} attemps")
-        break
+    secret_number = random.randint(1,100)
+    attempts = 0
+
+    while True:
+        try:
+            if attempts == 10: 
+                print("❌ You have reached the maximum number of attempts!")
+                print(f"The correct number was: {secret_number}")
+                break
+            else:
+                guess = int(input("Guess a number between 1 and 100: "))
+        except ValueError:
+            print("❌ Please Enter a valid number")
+            continue
+        
+        attempts += 1
+        
+        if guess < secret_number: 
+            print("Too Low! Try Again")
+        elif guess > secret_number:
+            print("Too High! Try Again")
+        else:
+            print(f"Congratulations you guessed it in {attempts} attempts")
+            break
+            
+    playAgain = input("Do you want to play again ? yes/no").strip().lower()
+    
+    if playAgain != "yes":
+        play = False
+        print("👋 Thanks for playing!")
